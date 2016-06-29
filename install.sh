9e400b4471acae7ba5cb7e41b9cd295d92d91a3e
Manisso <manisso@usa.com>
2016-06-29 08:58:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-echo "[ ]====================================================================[ ]";
-echo "[ ]                          Manisso Tools                             [ ]";
-echo "[ ]                      Xadmin Install Script                         [ ]";
-echo "[ ]====================================================================[ ]";
+echo "
+███╗   ███╗ █████╗ ███╗   ██╗██╗███████╗███████╗ ██████╗
+████╗ ████║██╔══██╗████╗  ██║██║██╔════╝██╔════╝██╔═══██╗
+██╔████╔██║███████║██╔██╗ ██║██║███████╗███████╗██║   ██║
+██║╚██╔╝██║██╔══██║██║╚██╗██║██║╚════██║╚════██║██║   ██║
+██║ ╚═╝ ██║██║  ██║██║ ╚████║██║███████║███████║╚██████╔╝
+╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚══════╝ ╚═════╝ 
+▀▀█▀▀ █▀▀█ █▀▀█ █   █▀▀ ~ Tools Instaler By Ⓜ Ⓐ Ⓝ Ⓘ Ⓢ Ⓢ Ⓞ  ☪ ~
+  █   █  █ █  █ █   ▀▀█ 
+  ▀   ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀             
+
+                                                ";
 echo "";
-echo "[!] Install.sh will install xadmin tool in the system [Y/n]" ; 
+echo "[✔] Install.sh will install xadmin tool in the system [Y/N] :" ;
 read baba
 if [ $baba == "y" ] ; 
   then
@@ -13,12 +21,12 @@ if [ $baba == "y" ] ;
     exit
 fi
 
-echo "[!] Where Do you want to install the tool? [Ex:/usr/share/doc]:";
+echo "[✔] Where Do you want to install the tool? [Ex:/usr/share/doc]:";
 read refdir
-echo "[!] Checking directories..."
+echo "[✔] Checking directories..."
 if [ -d "$refdir/Manisso-Tools" ] ;
 then
-echo "[!] A directory Manisso-Tools was found! Do you want to replace it? [Y/n]:" ; 
+echo "[◉] A directory Manisso-Tools was found! Do you want to replace it? [Y/n]:" ; 
 read mama
 if [ $mama == "y" ] ; 
 then
@@ -28,11 +36,11 @@ else
 fi
 fi
 
- echo "[!] Installing ...";
+ echo "[✔] Installing ...";
  echo "";
  git clone https://github.com/Manisso/Xadmin.git $refdir/Manisso-Tools;
  echo "#!/bin/bash 
- perl $refdir/ATSCAN/xadmin.pl" '${1+"$@"}' > xadmin;
+ perl $refdir/Manisso-Tools/xadmin.pl" '${1+"$@"}' > xadmin;
  chmod +x xadmin;
  sudo cp xadmin /usr/bin/;
  rm xadmin;
@@ -41,13 +49,13 @@ fi
 if [ -d "$refdir/Manisso-Tools" ] ;
 then
 echo "";
-echo "Tool istalled with success!";
+echo "[✔]Tool istalled with success![✔]";
 echo "";
-  echo "[ ]====================================================================[ ]";
-  echo "[ ]     All is done!! You can execute Xadmin by typing xadmin !        [ ]"; 
-  echo "[ ]====================================================================[ ]";
+  echo "[✔]====================================================================[✔]";
+  echo "[✔] ✔✔✔  All is done!! You can execute Xadmin by typing xadmin !   ✔✔✔ [✔]"; 
+  echo "[✔]====================================================================[✔]";
   echo "";
 else
-  echo "[!] Installation faid!! ";
+  echo "[✘] Installation faid![✘] ";
   exit
 fi
