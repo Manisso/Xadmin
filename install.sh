c0d83f2459fc5f081146b189f347c39d285a3294
Manisso <manisso@usa.com>
2016-06-29 17:36:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ echo "
 
                                                 ";
 echo "";
-echo "[✔] Install.sh will install xadmin tool in the system [Y/N] :" ;
+echo "[✔] You will install Xadmin tool in the system [Y/N] :" ;
 read baba
 if [ $baba == "y" ] ; 
   then
@@ -24,13 +24,13 @@ fi
 echo "[✔] Where Do you want to install the tool? [Ex:/usr/share/doc]:";
 read refdir
 echo "[✔] Checking directories..."
-if [ -d "$refdir/Manisso-Tools" ] ;
+if [ -d "$refdir/Xadmin" ] ;
 then
-echo "[◉] A directory Manisso-Tools was found! Do you want to replace it? [Y/n]:" ; 
+echo "[◉] A directory Xadmin was found! Do you want to replace it? [Y/n]:" ; 
 read mama
 if [ $mama == "y" ] ; 
 then
- rm -R "$refdir/Manisso-Tools"
+ rm -R "$refdir/Xadmin"
 else
  exit
 fi
@@ -38,15 +38,15 @@ fi
 
  echo "[✔] Installing ...";
  echo "";
- git clone https://github.com/Manisso/Xadmin.git $refdir/Manisso-Tools;
+ git clone https://github.com/Manisso/Xadmin.git $refdir/Xadmin;
  echo "#!/bin/bash 
- perl $refdir/Manisso-Tools/xadmin.pl" '${1+"$@"}' > xadmin;
+ perl $refdir/Xadmin/Xadmin.pl" '${1+"$@"}' > xadmin;
  chmod +x xadmin;
  sudo cp xadmin /usr/bin/;
  rm xadmin;
 
 
-if [ -d "$refdir/Manisso-Tools" ] ;
+if [ -d "$refdir/Xadmin" ] ;
 then
 echo "";
 echo "[✔]Tool istalled with success![✔]";
