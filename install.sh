6ba7738cc84860095eba079e3ecaf04c5c553370
Manisso <manisso@usa.com>
2016-06-29 08:33:07 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+echo "[ ]====================================================================[ ]";
+echo "[ ]                          Manisso Tools                             [ ]";
+echo "[ ]                      Xadmin Install Script                         [ ]";
+echo "[ ]====================================================================[ ]";
+echo "";
+echo "[!] Install.sh will install xadmin tool in the system [Y/n]" ; 
+read baba
+if [ $baba == "y" ] ; 
+  then
+    echo " "
+  else
+    exit
+fi
+
+echo "[!] Where Do you want to install the tool? [Ex:/usr/share/doc]:";
+read refdir
+echo "[!] Checking directories..."
+if [ -d "$refdir/Manisso-Tools" ] ;
+then
+echo "[!] A directory Manisso-Tools was found! Do you want to replace it? [Y/n]:" ; 
+read mama
+if [ $mama == "y" ] ; 
+then
+ rm -R "$refdir/Manisso-Tools"
+else
+ exit
+fi
+fi
+
+ echo "[!] Installing ...";
+ echo "";
+ git clone https://github.com/Manisso/Xadmin.git $refdir/Manisso-Tools;
+ echo "#!/bin/bash 
+ perl $refdir/ATSCAN/xadmin.pl" '${1+"$@"}' > xadmin;
+ chmod +x xadmin;
+ sudo cp xadmin /usr/bin/;
+ rm xadmin;
+
+
+if [ -d "$refdir/Manisso-Tools" ] ;
+then
+echo "";
+echo "Tool istalled with success!";
+echo "";
+  echo "[ ]====================================================================[ ]";
+  echo "[ ]     All is done!! You can execute Xadmin by typing xadmin !        [ ]"; 
+  echo "[ ]====================================================================[ ]";
+  echo "";
+else
+  echo "[!] Installation faid!! ";
+  exit
+fi
